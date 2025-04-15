06c3beca004aad0a630316298d8ec0c864660030
Leane Schlundt <lhecht@techfak.de>
2025-04-15 12:17:10 +0000

    Make exception message more understandable

--- src/resources/scripts/server.py
+++ src/resources/scripts/server.py
@@ -7,21 +7,35 @@ import os
 from sam import Sam
 from microservice import process_requests, generate_response
 
-url = os.environ.get('MAGIC_SAM_MODEL_URL')
-path = os.environ.get("CHECKPOINT_PATH")
-model_type = os.environ.get('MAGIC_SAM_MODEL_TYPE')
-device = os.environ.get('MAGIC_SAM_DEVICE')
+MAGIC_SAM_MODEL_URL = 'MAGIC_SAM_MODEL_URL'
+MAGIC_SAM_CHECKPOINT_PATH = 'MAGIC_SAM_CHECKPOINT_PATH'
+MAGIC_SAM_MODEL_TYPE = 'MAGIC_SAM_MODEL_TYPE'
+MAGIC_SAM_DEVICE = 'MAGIC_SAM_DEVICE'
+
+url = os.environ.get(MAGIC_SAM_MODEL_URL)
+path = os.environ.get(MAGIC_SAM_CHECKPOINT_PATH)
+model_type = os.environ.get(MAGIC_SAM_MODEL_TYPE)
+device = os.environ.get(MAGIC_SAM_DEVICE)
+
+
+def is_checkpoint_path():
+    return path.endswith('.pth')
+
 
 try:
-    if (not url or not '.pth' in path or not model_type or not device):
-        # überarbeiten
-        raise Exception("Magic-Sam environment variables are missing.")
+    if (not url or not is_checkpoint_path() or not model_type or not device):
+        raise Exception("Couldn't initialize python server.\n\tMissing env-variables: {u}{p}{t}{d}".format(
+            u=MAGIC_SAM_MODEL_URL+', ' if not url else '',
+            p=MAGIC_SAM_CHECKPOINT_PATH+', ' if not is_checkpoint_path() else '',
+            t=MAGIC_SAM_MODEL_TYPE+', ' if not model_type else '',
+            d=MAGIC_SAM_DEVICE if not device else ''))
     sam = Sam(url, path, model_type, device)
     threading.Thread(target=process_requests, daemon=True, args=[sam]).start()
     app = Flask(__name__)
 except Exception as e:
     logging.error(e)
     sys.exit(4)
+
 
 @app.route("/embedding", methods=['POST'])
 def index():
