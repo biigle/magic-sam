93820a5c9e864a25f11ed84806fef8b1426eb58d
Leane Schlundt <lhecht@techfak.de>
2025-04-17 15:35:27 +0000

    Return embedding if available

--- src/Http/Controllers/ImageEmbeddingController.php
+++ src/Http/Controllers/ImageEmbeddingController.php
@@ -2,12 +2,15 @@
 
 namespace Biigle\Modules\MagicSam\Http\Controllers;
 
-use Biigle\Http\Controllers\Api\Controller;
 use Biigle\Image;
-use Biigle\Modules\MagicSam\Jobs\GenerateEmbedding;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
+use Biigle\Http\Controllers\Api\Controller;
+use Biigle\Modules\MagicSam\Jobs\GenerateEmbedding;
 
 class ImageEmbeddingController extends Controller
 {
@@ -29,7 +32,6 @@ class ImageEmbeddingController extends Controller
      *
      * @param Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request, $id)
     {
@@ -38,21 +40,33 @@ class ImageEmbeddingController extends Controller
 
         $disk = Storage::disk(config('magic_sam.embedding_storage_disk'));
         $filename = "{$image->id}.npy";
-        $url = null;
+        $embedding = null;
         if ($disk->exists($filename)) {
-            if ($disk->providesTemporaryUrls()) {
-                $url = $disk->temporaryUrl($filename, now()->addHour());
-            } else {
-                $url = $disk->url($filename);
-            }
+            $path = $disk->path($filename);
+            // Embedding needs to be encoded because binary would be corrupted
+            $embedding = base64_encode(File::get($path));
         } else {
-            Queue::connection(config('magic_sam.request_connection'))
+            $job_count = config('magic_sam.job_count_cache_key');
+            if (!Cache::has($job_count)) {
+                Cache::add($job_count, 1);
+            }
+
+            if (Cache::get($job_count) > config('magic_sam.queue_threshold')) {
+                Queue::connection(config('magic_sam.request_connection'))
                 ->pushOn(
                     config('magic_sam.request_queue'),
-                    new GenerateEmbedding($image, $request->user())
-                );
+                    new GenerateEmbedding($image, $request->user()));
+                   $embedding = null;
+            } else {
+                // Call handle() directly instead of dispatchSync() to save time
+                $job = new GenerateEmbedding($image, $request->user(), False);
+                $job->handle();
+                $path = $disk->path($filename);
+                // Embedding needs to be encoded because binary would be corrupted
+                $embedding = base64_encode(File::get($path));
+            }
         }
 
-        return ['url' => $url];
+        return response()->json(['embedding' => $embedding]);
     }
 }
