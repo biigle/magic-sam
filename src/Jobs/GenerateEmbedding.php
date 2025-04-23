fd0898a5ada726ea3977f35e6cc6fe2cc6ae9ef8
Leane Schlundt <lhecht@techfak.de>
2025-04-23 14:19:36 +0000

    Fix comment

--- src/Jobs/GenerateEmbedding.php
+++ src/Jobs/GenerateEmbedding.php
@@ -75,7 +75,7 @@ class GenerateEmbedding
         $embeddingFilename = "{$this->image->id}.npy";
         $disk = Storage::disk(config('magic_sam.embedding_storage_disk'));
         try {
-            // Check whether file exists again, because job can be executed in app or pyworker container
+            // Check whether file exists again, because job can be executed in app or worker container
             if (!$disk->exists($embeddingFilename)) {
                 $this->generateEmbedding($embeddingFilename, $disk->path($embeddingFilename));
             }
