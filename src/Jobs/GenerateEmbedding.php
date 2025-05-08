75949c4c4553352ae1306b066ab5bb9fb5c42860
Leane Schlundt <lhecht@techfak.de>
2025-05-08 14:23:52 +0000

    Crop and resize images

--- src/Jobs/GenerateEmbedding.php
+++ src/Jobs/GenerateEmbedding.php
@@ -7,9 +7,12 @@ use FileCache;
 use Biigle\User;
 use Biigle\Image;
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+use Jcupitt\Vips\Image as VipsImage;
 use Illuminate\Support\Facades\Cache;
+use Biigle\Modules\MagicSam\Embedding;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 use Biigle\Modules\MagicSam\Events\EmbeddingFailed;
@@ -41,6 +44,18 @@ class GenerateEmbedding
     public $isAsync;
 
     /**
+     * Lower left and upper right corner of viewport
+     * @var array
+     */
+    public $extent;
+
+    /**
+     * The embedding of the image (section)
+     * @var Embedding
+     */
+    public $embedding;
+
+    /**
      * Ignore this job if the image or user does not exist any more.
      *
      * @var bool
@@ -54,11 +69,12 @@ class GenerateEmbedding
      * @param User $user
      * @param bool $isAsync
      */
-    public function __construct(Image $image, User $user, bool $isAsync = True)
+    public function __construct(Image $image, User $user, bool $isAsync = True, $extent = [])
     {
         $this->image = $image;
         $this->user = $user;
         $this->isAsync = $isAsync;
+        $this->extent = $extent;
     }
 
     /**
@@ -72,14 +88,31 @@ class GenerateEmbedding
             Cache::increment(config('magic_sam.job_count_cache_key'));
         }
 
-        $embeddingFilename = "{$this->image->id}.npy";
         $disk = Storage::disk(config('magic_sam.embedding_storage_disk'));
+        $prefix = fragment_uuid_path($this->image->uuid);
+
+        $emb = new Embedding;
+        $emb->image_id = $this->image->id;
+        $emb->x = $this->extent[0];
+        $emb->y = $this->extent[1];
+        $emb->x2 = $this->extent[2];
+        $emb->y2 = $this->extent[3];
+
+        $filenameHash = $emb->getFilenameHash();
+        $embFilename = "{$filenameHash}.npy";
+
+        $emb->filename = $embFilename;
+
+        if(!$disk->exists($prefix)) {
+            $disk->makeDirectory($prefix);
+        }
+
         try {
-            $this->generateEmbedding($embeddingFilename, $disk->path($embeddingFilename));
+            $this->generateEmbedding($emb, $embFilename, $disk->path("{$prefix}/{$embFilename}"));
 
             if ($this->isAsync) {
                 $this->decrementJobCacheCount();
-                EmbeddingAvailable::dispatch($embeddingFilename, $this->user);
+                EmbeddingAvailable::dispatch($embFilename, $this->user);
             }
         } catch (Exception $e) {
             if ($this->isAsync) {
@@ -112,16 +145,18 @@ class GenerateEmbedding
      * 
      * @return string embedding as binary
      */
-    protected function generateEmbedding($embeddingFilename, $destPath)
+    protected function generateEmbedding($emb, $embeddingFilename, $destPath)
     {
-        return FileCache::getOnce($this->image, function ($file, $path) use ($embeddingFilename, $destPath) {
+        return FileCache::getOnce($this->image, function ($file, $path) use ($emb, $embeddingFilename, $destPath) {
+            // (crop and) resize image
+            $image = $this->processImage($path);
             // Contact the pyworker to generate the embedding
             $response = Http::withOptions([
                 'sink' => $destPath // stream response body to disk
             ])
                 ->attach(
                     'image',
-                    File::get($path),
+                    $image,
                     $file->filename
                 )
                 ->post('http://pyworker:8080/embedding', ['filename' => $embeddingFilename]);
@@ -129,7 +164,56 @@ class GenerateEmbedding
             if (!$response->successful()) {
                 throw new Exception("The image couldn't be processed by the Magic-Sam tool. Please try again.");
             }
-            return;
+
+            $this->embedding = DB::transaction(function () use ($emb) {
+                $emb->save();
+                return $emb;
+            });
         });
     }
+
+    protected function processImage($path)
+    {
+        $width = $this->image->width;
+        $height = $this->image->height;
+        $format = pathinfo($this->image->filename, PATHINFO_EXTENSION);
+
+        // cut image section
+        if ($this->extent) {
+            $width = floor(abs($this->extent[0] - $this->extent[2]));
+            $height = floor(abs($this->extent[1] - $this->extent[3]));
+
+            $image = VipsImage::newFromFile($path); // TODO: does this work with large images ??
+            $image = $image->crop($this->extent[0], $this->extent[1], $width, $height);
+            $filenameHash = md5(serialize([$this->image->id, ...$this->extent]));
+            $path = sys_get_temp_dir() . "/tmp_{$filenameHash}.{$format}";
+            $image->writeToFile($path); // TODO: alternative ohne speichern?
+        }
+
+        // resize image
+        $targetSize = 1024;
+
+        if ($width > $height) {
+            $height = ($height / $width) * $targetSize;
+            $width = $targetSize;
+        } else {
+            $width = ($width / $height) * $targetSize;
+            $height = $targetSize;
+        }
+
+        $buffer = $image->thumbnail($path, $width, [
+            'height' => $height,
+            // Don't auto rotate thumbnails because the orientation of AUV captured
+            // images is not reliable.
+            'no-rotate' => true,
+        ])->writeToBuffer(".{$format}", [
+                    'Q' => 85,
+                    'strip' => true,
+                ]);
+
+        unlink($path); // check if true
+
+        return $buffer;
+    }
+
 }
