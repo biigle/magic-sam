6459d74f4a4a1085238dc05ef309265f5fc3dd7c
Leane Schlundt <lhecht@techfak.de>
2025-05-10 12:32:47 +0000

    Link factory with model

--- src/Database/factories/EmbeddingFactory.php
+++ src/Database/factories/EmbeddingFactory.php
@@ -3,6 +3,7 @@
 namespace Biigle\Modules\MagicSam\Database\factories;
 
 use Biigle\Image;
+use Biigle\Modules\MagicSam\Embedding;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,6 +11,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class EmbeddingFactory extends Factory
 {
+    protected $model = Embedding::class;
+
     /**
      * Define the model's default state.
      *
