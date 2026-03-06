6ffd76dabb2ecf228e0c9734c5b98c38fc50768f
Martin Zurowietz <martin@zurowietz.de>
2026-03-06 16:52:34 +0100

    Update manual for detailed mode

--- src/resources/views/manualAnnotationShortcutsPolygon.blade.php
+++ src/resources/views/manualAnnotationShortcutsPolygon.blade.php
@@ -1,4 +1,6 @@
 <tr>
     <td><kbd>z</kbd></td>
-    <td>Select the magic SAM tool</td>
+    <td>
+        Select the magic SAM tool. Press <kbd>z</kbd> again to enable <a href="/manual/tutorials/annotations/creating-annotations#magic-sam">detailed mode</a>.
+    </td>
 </tr>
